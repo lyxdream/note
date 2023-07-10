daef80bb3468846064b751df043b11e0ceab86a0
yinxia <liyinxia@weimiao.cn>
2023-07-10 18:17:42 +0800

    feat: 闭包的更多作用

--- js基础/高程/this对象.md
+++ js基础/高程/this对象.md
@@ -99,3 +99,42 @@ console.log(obj.name)  //sven
   console.log( obj1.getName() );     // 输出： sven
   console.log( obj1.getName.call( obj2 ) );    // 输出：anne
 ```
+
+## 闭包中使用this
+
+```js
+window.identity = 'The Window'; 
+let object = { 
+    identity: 'My Object', 
+    getIdentityFunc() { 
+        return function() { 
+        return this.identity; 
+    }; 
+ } 
+}; 
+console.log(object.getIdentityFunc()()); // 'The Window'
+```
+
+匿名函数返回 this.identity。因为getIdentityFunc()返回函数，所以 object.getIdentityFunc()()会立即调用这个返回的函数，从而得到一个字符串。可是，此时返回的字符串是"The Winodw"，即全局变量 identity 的值。么匿名函数没有使用其包含作用域（getIdentityFunc()）的 this 对象呢？
+
+
+> 原因：每个函数在被调用时都会自动创建两个特殊变量：this 和arguments。内部函数永远不可能直接访问外部函数的这两个变量。
+但是，如果把 this 保存到闭包可以访问的另一个变量中，
+则是行得通的。
+
+```js
+window.identity = 'The Window'; 
+let object = { 
+ identity: 'My Object', 
+ getIdentityFunc() { 
+    let that = this; 
+    return function() { 
+        return that.identity; 
+    }; 
+ } 
+}; 
+console.log(object.getIdentityFunc()()); // 'My Object'
+```
+> 即使在外部函数返回之后，that 仍然指向 object，所以调用 object.getIdentityFunc()()就会返回"My Obj
+
+> 注意 this 和 arguments 都是不能直接在内部函数中访问的。如果想访问包含作用域中的 arguments 对象，则同样需要将其引用先保存到闭包能访问的另一个变量中。
--- test.js
+++ test.js
@@ -1,24 +1,116 @@
-function fn2(){
-  console.log(a)
-}
-function fn1(){
-	const a = 1
-  fn2()
-}
-const a = 2
+// function fn2(){
+//   console.log(a)
+// }
+// function fn1(){
+// 	const a = 1
+//   fn2()
+// }
+// const a = 2
 // function fn2(){
 //   console.log(a)
 // }
 // function fn3(f){
 //   const a = 4;
-//   f()      
+//   f()
 // }
-fn1()
+// fn1()
 // fn3(fn2)
-
 
 // function fn3(){
 //   const a = 4;
 //   console.log(a)
 // }
 // fn3()
+// var cache = {};
+
+// var mult = function(){
+//   console.log(arguments,'==arguments')
+//     var args = Array.prototype.join.call( arguments, ', ' );
+//     console.log(args,'==args')
+//     if ( cache[ args ] ){
+//       return cache[ args ];
+//     }
+//     var a = 1;
+//     for ( var i = 0, l = arguments.length; i < l; i++ ){
+//         a = a * arguments[i];
+//     }
+
+//     return cache[ args ] = a;
+// };
+
+// console.log ( cache,mult( 1,2,3 ) );     // 输出：6
+
+
+
+// function  fn1() {
+//   let num = 1;
+//   return function () {
+//       /*fn1=>num*/
+//       num++;
+//       console.log(num)
+//   }
+// }
+// let fn2 = fn1(); 
+// fn2();//2
+// fn2();//3
+
+// let fn3 = fn1();
+// fn3()//2
+// var num = 1;
+// function  fn1() {
+//   let  num = 5;
+//     return function () {
+//       console.log(num);
+//       num++
+//     }
+// }
+
+// var fn2 =fn1();
+// num = 2;
+// fn2()   
+
+
+
+// let mult = function(){
+   
+// };
+
+
+
+// let mult = (function(){
+//   let cache = {};
+//   return function(){
+//     const args = Array.prototype.join.call( arguments, ', ' );  //1, 2, 3
+//     if ( cache[ args ] ){
+//       return cache[ args ];
+//     }
+//     let a = 1;
+//     for ( let i = 0, l = arguments.length; i < l; i++ ){
+//         a = a * arguments[i];
+//     }
+//     return cache[ args ] = a;
+//   }
+// })()
+// console.log ( mult( 1,2,3 ) );     // 输出：6
+// console.log ( mult( 1,2,3 ) );     // 输出：6
+
+
+
+let mult = (function(){
+  let cache = {};
+  const calculate = function(){
+    let a = 1;
+    for ( let i = 0, l = arguments.length; i < l; i++ ){
+        a = a * arguments[i];
+    }
+    return a
+  }
+  return function(){
+    const args = Array.prototype.join.call( arguments, ', ' );  //1, 2, 3
+    if ( cache[ args ] ){
+      return cache[ args ];
+    }
+    return cache[ args ] = calculate.apply(null,arguments);
+  }
+})()
+console.log ( mult( 1,2,3 ) );     // 输出：6
